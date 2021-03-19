7f4b6bd6c7281ac4e5f31cd14599faceb60347a7
keyisfocus <keyisfocus@gmail.com>
2021-03-19 19:22:35 +0300

    Stage 7 done

--- calculator/Main.java
+++ calculator/Main.java
@@ -1,52 +1,102 @@
 package calculator;
 
-import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Main {
-    private static Map<String, Integer> vars = new HashMap<>();
+    private static final Map<String, Integer> vars = new HashMap<>();
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
 
         try (var scan = new Scanner(System.in)) {
             while (true) {
-                var line = scan.nextLine().strip();
+                try {
+                    var line = scan.nextLine().strip();
 
-                if (line.startsWith("/")) {
-                    executeCommand(line);
-                    continue;
-                }
-
-                if (line.isEmpty()) {
-                    continue;
-                }
-
-                if (line.contains("=")) {
-                    assignment(line);
-                    continue;
-                }
-
-                if (!line.matches("[-+]?([0-9]|[a-zA-Z])+(\\s+[-+]+\\s+[-+]?([0-9]|[a-zA-Z])+)*")) {
-                    System.out.println("Invalid expression");
-                    continue;
-                }
-
-                var nums = new ArrayList<Integer>();
-                var signs = new ArrayList<Character>();
-
-                if (parse(line, nums, signs)) {
-
-                    var res = nums.get(0);
-                    for (int i = 0; i < signs.size(); i++) {
-                        var n = nums.get(i + 1);
-                        var s = signs.get(i);
-                        res = s == '-' ? res - n : res + n;
+                    if (line.startsWith("/")) {
+                        executeCommand(line);
+                        continue;
                     }
+
+                    if (line.isEmpty()) {
+                        continue;
+                    }
+
+                    if (line.contains("=")) {
+                        assignment(line);
+                        continue;
+                    }
+
+                    if (!line.matches("[0-9a-zA-Z-+/*\\s=()]+")) {
+                        throw new IllegalArgumentException("Invalid expression");
+                    }
+
+                    var operand = new StringBuilder();
+                    var operator = new StringBuilder();
+
+                    var stack = new ArrayDeque<Character>();
+                    var postfix = new StringBuilder();
+
+                    for (int i = 0; i < line.length(); i++) {
+                        var c = line.charAt(i);
+                        if (Character.isWhitespace(c)) {
+                            continue;
+                        }
+
+                        if (Character.isLetterOrDigit(c)) {
+                            if (operator.length() != 0) {
+                                if (operator.toString().equals("-") && line.charAt(i - 1) != ' ') {
+                                    operand.append('-');
+                                } else {
+                                    parseOperator(operator, stack, postfix);
+                                }
+                                operator.setLength(0);
+                            }
+                            operand.append(c);
+                        } else {
+                            if (operand.length() != 0) {
+                                postfix.append(operand).append(" ");
+                                operand.setLength(0);
+                            }
+                            if (c == '-' || c == '+') {
+                                if (!operator.toString().matches("[-+]*")) {
+                                    parseOperator(operator, stack, postfix);
+                                    operator.setLength(0);
+                                }
+                            } else if (operator.length() != 0) {
+                                parseOperator(operator, stack, postfix);
+                                operator.setLength(0);
+                            }
+                            operator.append(c);
+                        }
+                    }
+                    if (operand.length() != 0) {
+                        postfix.append(operand).append(" ");
+                    }
+                    if (operator.length() != 0) {
+                        parseOperator(operator, stack, postfix);
+                    }
+
+                    while (!stack.isEmpty()) {
+                        var o = stack.pop();
+                        if (o == '(' || o == ')') {
+                            throw new IllegalArgumentException("Invalid expression");
+                        }
+                        postfix.append(o).append(" ");
+                    }
+                    postfix.deleteCharAt(postfix.length() - 1);
+
+                    var res = calculate(postfix.toString().strip());
                     System.out.println(res);
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                } catch (NoSuchElementException e) {
+                    System.out.println("Invalid expression");
                 }
             }
         }
@@ -61,7 +111,7 @@ public class Main {
                 System.out.println("The program calculates given expression");
                 break;
             default:
-                System.out.println("Unknown command");
+                throw new IllegalArgumentException("Unknown command");
         }
     }
 
@@ -72,43 +122,54 @@ public class Main {
 
         if (line.indexOf('=') != line.lastIndexOf('=') ||
                 !parts[0].matches("[a-zA-Z]+") ||
-                !parts[1].matches("([0-9]+|[a-zA-Z]+)")) {
-            System.out.println("Invalid expression");
-            return;
+                !parts[1].matches("-?([0-9]+|[a-zA-Z]+)")) {
+            throw new IllegalArgumentException("Invalid expression");
         }
-        if (parts[1].matches("[0-9]+")) {
+        if (parts[1].matches("-?[0-9]+")) {
             vars.put(parts[0], Integer.parseInt(parts[1]));
         } else {
             if (vars.containsKey(parts[1])) {
-                vars.put(parts[0], vars.get(parts[1]));
-            } else {
-                System.out.println("Invalid expression");
-                return;
-            }
-        }
-    }
-
-    private static boolean parse(String expression, List<Integer> nums, List<Character> signs) {
-        var parts = expression.split("\\s+");
-
-        for (String part : parts) {
-            if (part.matches("[0-9]+")) {
-                nums.add(Integer.parseInt(part));
-            } else if (part.matches("[a-zA-Z]+")) {
-                if (vars.containsKey(part)) {
-                    nums.add(vars.get(part));
-                } else {
-                    System.out.println("Unknown variable");
-                    return false;
+                var v = vars.get(parts[1]);
+                if (parts[1].charAt(0) == '-') {
+                    v = -v;
                 }
+                vars.put(parts[0], v);
             } else {
-                signs.add(parseSign(part));
+                throw new IllegalArgumentException("Unknown variable");
             }
         }
-        return true;
     }
 
-    private static char parseSign(String s) {
+
+    private static void parseOperator(CharSequence s, Deque<Character> stack, StringBuilder sb) {
+        char c;
+        if (s.length() != 1) {
+            c = parseSign(s);
+        } else {
+            c = s.charAt(0);
+        }
+
+        if (c == ')') {
+            char i;
+            while ((i = stack.pop()) != '(') {
+                sb.append(i).append(" ");
+            }
+        } else if (stack.isEmpty() ||
+                stack.peek() == '(' ||
+                getPrecedence(c) > getPrecedence(stack.peek()) ||
+                c == '(') {
+            stack.push(c);
+        } else if (getPrecedence(c) <= getPrecedence(stack.peek())) {
+            while (!stack.isEmpty() &&
+                    stack.peek() != '(' &&
+                    getPrecedence(stack.peek()) >= getPrecedence(c)) {
+                sb.append(stack.pop()).append(" ");
+            }
+            stack.push(c);
+        }
+    }
+
+    private static char parseSign(CharSequence s) {
         boolean plus = true;
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == '-') {
@@ -116,5 +177,63 @@ public class Main {
             }
         }
         return plus ? '+' : '-';
+    }
+
+    private static int getPrecedence(char c) {
+        switch (c) {
+            case '-':
+            case '+':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+            default:
+                return -1;
+        }
+    }
+
+    private static int calculate(String postfix) {
+        var stack = new ArrayDeque<Integer>();
+        for (String s : postfix.split("\\s+")) {
+            if (s.matches("-?[0-9]+")) {
+                stack.push(Integer.parseInt(s));
+            } else if (s.matches("-?[a-zA-Z]+")) {
+                if (vars.containsKey(s)) {
+                    var v = vars.get(s);
+                    if (s.charAt(0) == '-') {
+                        v = -v;
+                    }
+                    stack.push(v);
+                } else {
+                    throw new IllegalArgumentException("Unknown variable");
+                }
+            } else {
+                var y = stack.pop();
+                var x = stack.pop();
+                var r = performOperation(s, x, y);
+                stack.push(r);
+            }
+        }
+        var res = stack.pop();
+        if (!stack.isEmpty()) {
+            throw new IllegalArgumentException("Invalid expression");
+        } else {
+            return res;
+        }
+    }
+
+    private static int performOperation(String operator, int x, int y) {
+        switch (operator) {
+            case "+":
+                return x + y;
+            case "-":
+                return x - y;
+            case "*":
+                return x * y;
+            case "/":
+                return x / y;
+            default:
+                throw new IllegalArgumentException("Invalid expression");
+        }
     }
 }
